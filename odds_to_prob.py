ccd558a66ab1c2c8098fb5261dba9307e373244a
unknown <adamparker05@gmail.com>
2024-11-30 07:13:36 -0500

    added bet amount calculator and remade exe

--- odds_to_prob.py
+++ odds_to_prob.py
@@ -8,6 +8,7 @@ def convert(self):
     odds_type = odds_combobox.get()
     odds_flt = float(odds.get())
     real_odds_flt = float(real_odds.get())
+    roll = float(bankroll.get())
 
     if odds_type == 'American':
         if odds_flt < 0:
@@ -24,9 +25,14 @@ def convert(self):
         prop_gain = 1 / odds_flt - 1
         prob.set(str(res))
 
+    k_crit = (real_odds_flt - (1-real_odds_flt) / prop_gain) * 100
+    bet_amt = roll * k_crit
     k_crit = round(Decimal((real_odds_flt - (1-real_odds_flt) / prop_gain) * 100), 2)
-    
+    bet_amt = round(Decimal(bet_amt / 100), 2)
+
     kelly.set(str(k_crit) + '%')
+    bet.set(str(bet_amt))
+    
 
 
 
@@ -44,6 +50,8 @@ odds = StringVar(root)
 prob = StringVar(root)
 real_odds = StringVar(root)
 kelly = StringVar(root)
+bankroll = StringVar(root)
+bet = StringVar(root)
 
 ttk.Label(frame, text='Odds: ').grid(column=0, row=0, sticky=W)
 odds_value = ttk.Entry(frame, textvariable=odds).grid(column=1, row=0, sticky=W)
@@ -54,10 +62,19 @@ odds_combobox.current(0)
 
 ttk.Label(frame, text='Prob(1-?): ').grid(column=0, row=1, sticky=W)
 ttk.Label(frame, textvariable=prob).grid(column=1, row=1, sticky=W)
+
 ttk.Label(frame, text='Real Prob: ').grid(column=0, row=2, sticky=W)
 real_odds_value = ttk.Entry(frame, textvariable=real_odds).grid(column=1, row=2, sticky=W)
+
 ttk.Label(frame, text='Kelly: ').grid(column=0, row=4, sticky=W)
 ttk.Label(frame, textvariable=kelly).grid(column=1, row=4, sticky=W)
+
+ttk.Label(frame, text='Bankroll: ').grid(column=0, row=5, sticky=W)
+bankroll_value = ttk.Entry(frame, textvariable=bankroll).grid(column=1, row=5, sticky=W)
+
+ttk.Label(frame, text='Bet: ').grid(column=0, row=6, sticky=W)
+ttk.Label(frame, textvariable=bet).grid(column=1, row=6, sticky=W)
+
 
 root.bind('<Return>', convert)
 
